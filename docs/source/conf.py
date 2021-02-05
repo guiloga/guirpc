9db25cfd85dc0dce9dd5e34de0aac6011e4367c8
Guillem López Garcia <wyl.loga@gmail.com>
2021-02-05 11:49:51 +0100

    Changed initial version 0.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Guillem López Garcia'
 author = 'Guillem López Garcia'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.1'
 
 master_doc = 'index'
 
